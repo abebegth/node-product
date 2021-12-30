03c43783aaaaefed2a11f38dbfbe0c8502ee7cd8
Abebe Gietaneh <abebegth@gmail.com>
2021-12-30 21:58:20 +0300

    Creating a web server

--- index.js
+++ index.js
@@ -3,24 +3,36 @@ const http = require('http');
 
 ///////////////// FILES ////////////////
 
-// Synchronous way, Blocking
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
-const textOut = `This is what we know about the avocado: ${textIn}. \n Created on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written');
+// // Synchronous way, Blocking
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}. \n Created on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written');
 
-// Asynchronous, Non-blocking
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) =>{
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) =>{
-        console.log(data2);
-        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) =>{
-            console.log(data3)
+// // Asynchronous, Non-blocking
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) =>{
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) =>{
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) =>{
+//             console.log(data3)
             
-            fs.writeFile('./txt/final.txt', `${data2}\n ${data3}`, 'utf-8', err =>{
-                console.log("Your file has been written.")
-            })
-        });
-    });
+//             fs.writeFile('./txt/final.txt', `${data2}\n ${data3}`, 'utf-8', err =>{
+//                 console.log("Your file has been written.")
+//             })
+//         });
+//     });
+// });
+// console.log("Reading FIle...");
+
+
+////////////////////// SERVER ///////////////
+
+const server = http.createServer((req, res) =>{
+    console.log(req);
+    res.end('Hello from the server!');
+
 });
-console.log("Reading FIle...");
+server.listen(8000, '127.0.0.1', () =>{
+    console.log('Listening to requests on http://127.0.0.1:8000');
+});
