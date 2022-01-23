ba8ffdeb6e65721abe1d556bcc9423004ad5c9ae
Abebe Gietaneh <abebegth@gmail.com>
2022-01-23 18:07:58 +0300

    move the replaceTemplate function to a module and import it in index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const replaceTemplate = require('./modules/replaceTemplate');
+
 ///////////////// FILES ////////////////
 
 // // Synchronous way, Blocking
@@ -28,24 +30,6 @@ const url = require('url');
 
 
 ////////////////////// SERVER ///////////////
-
-const replaceTemplate = (temp, product) =>{
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic){
-        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    }
-
-    return output;
-
-}
 
 // this functions will be executed only once (synchronously) when the application starts, then the data read from this function 
 //can be used as many as we want without calling this function..... read once use multiple times in other functions
